7317fb75f06e35d16ca27cf58e059aaf880e708d
Derek Eder <derek.eder@gmail.com>
2012-06-10 21:32:14 -0500

    resolved logo issues

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,42 +1,33 @@
 $(function() {
-  $("#pickUpDate").datepicker();
+  $("#pickUpDates").datepicker();
   
   $('#addDate').click(function() {
       var num     = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
       var newNum  = new Number(num + 1);      // the numeric ID of the new input field being added
 
+      // business rule: you can only add 5 names
+      if (newNum == 6) return false;
+      
       // create the new element via clone(), and manipulate it's ID using newNum value
-      var newElem = $('#pickUpDate' + num).clone().attr('id', 'pickUpDate' + newNum);
+      var newElem = $('#dateElement' + num).clone().attr('id', 'dateElement' + newNum);
 
       // manipulate the name/id values of the input inside the new element
-      newElem.children(':first').attr('id', 'pickUpDate' + newNum).attr('pickUpDate', 'pickUpDate' + newNum);
+      //newElem.children(':first').attr('id', 'pickUpDate' + newNum).attr('pickUpDate', 'pickUpDate' + newNum);
 
       // insert the new element after the last "duplicatable" input field
-      $('#pickUpDate' + num).after(newElem);
-
-      // enable the "remove" button
-      $('#delDate').attr('disabled','');
-
-      // business rule: you can only add 5 names
-      if (newNum == 5)
-          $('#addDate').attr('disabled','disabled');
-          
+      $('#dateElement' + num).after(newElem);
+ 
       return false;
   });
 
   $('#delDate').click(function() {
       var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-      $('#pickUpDate' + num).remove();     // remove the last element
-
-      // enable the "add" button
-      $('#addDate').attr('disabled','');
-
+      
       // if only one element remains, disable the "remove" button
-      if (num-1 == 1)
-          $('#delDate').attr('disabled','disabled');
-          
+      if (num-1 == 0) return false;
+      
+      $('#dateElement' + num).remove();     // remove the last element
+
       return false;
   });
-
-  $('#delDate').attr('disabled','disabled');
 });
